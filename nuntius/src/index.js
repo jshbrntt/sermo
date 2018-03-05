5a24b115ee74131c714471cfbc44c29f0ec79beb
Joshua Barnett <jbacontrol@gmail.com>
2018-03-05 22:59:40 +0000

    Remove temporary server communication test code, and references to App boilerplate component in the client entry point.

--- nuntius/src/index.js
+++ nuntius/src/index.js
@@ -1,17 +1,5 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 import './index.css'
-import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 
-import io from 'socket.io-client'
-const socket = io('http://localhost:3001')
-
-socket.emit('message', { content: 'Hello World!' })
-
-socket.on('message', data => {
-  console.log('received message', data)
-})
-
-ReactDOM.render(<App />, document.getElementById('root'))
 registerServiceWorker()
