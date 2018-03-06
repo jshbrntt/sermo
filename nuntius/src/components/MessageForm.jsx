c59cbe9cb69079a61786d3d33b135ab830e04f40
Joshua Barnett <jbacontrol@gmail.com>
2018-03-06 23:44:08 +0000

    Fix clearing of the message form upon submission.

--- nuntius/src/components/MessageForm.jsx
+++ nuntius/src/components/MessageForm.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export function MessageForm ({ onSubmit, onChange }) {
+export function MessageForm ({ input, onSubmit, onChange }) {
   return (
     <form onSubmit={onSubmit}>
-      <input type='text' onChange={onChange} />
+      <input value={input} type='text' onChange={onChange} />
       <button type='submit'>
         Send
       </button>
