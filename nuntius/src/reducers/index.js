1310d36a62438552c5174e8e629af5297d7cfa82
Joshua Barnett <jbacontrol@gmail.com>
2018-03-07 21:39:13 +0000

    Add new reducer to index.

--- nuntius/src/reducers/index.js
+++ nuntius/src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux'
 import { routerReducer } from 'react-router-redux'
 import form from './form'
 import messages from './messages'
+import commands from './commands'
 
 export default combineReducers({
   routing: routerReducer,
   form,
-  messages
+  messages,
+  commands
 })
