fda5bf8c0dacf38747500d69670c8590973be8fc
Joshua Barnett <jbacontrol@gmail.com>
2018-03-07 20:09:34 +0000

    Add basic MessageForm styling.

--- nuntius/src/components/MessageForm.jsx
+++ nuntius/src/components/MessageForm.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 export function MessageForm ({ input, onSubmit, onChange }) {
   return (
     <form className='MessageForm' onSubmit={onSubmit}>
-      <input value={input} type='text' onChange={onChange} />
+      <input placeholder='Type a message' value={input} type='text' onChange={onChange} />
       <button type='submit'>
         Send
       </button>
