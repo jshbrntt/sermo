b7b18b518cdd6e34b3c3f40918ca9de9851790d5
Joshua Barnett <jbacontrol@gmail.com>
2018-03-07 19:12:04 +0000

    Add basic styling of the MessageList.
    https://codepen.io/fauxparse/pen/ebvEG

--- nuntius/src/components/MessageList.jsx
+++ nuntius/src/components/MessageList.jsx
@@ -5,7 +5,7 @@ export function MessageList ({ messages }) {
   return (
     <ul className='MessageList'>
       {messages.map((message, index) => (
-        <li key={index}>
+        <li className={message.status === 'received' ? 'them' : 'me'} key={index}>
           [{index}]: {message.content} ({message.status})
         </li>
       ))}
