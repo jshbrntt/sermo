5d61f5be2d45b17abca7cd46293a8a8fe3a41d48
Joshua Barnett <jbacontrol@gmail.com>
2018-03-08 22:05:08 +0000

    Refactor message view into its own Message component.

--- nuntius/src/components/MessageList.jsx
+++ nuntius/src/components/MessageList.jsx
@@ -1,19 +1,13 @@
 import './MessageList.scss'
 import React from 'react'
-import classNames from 'classnames'
+import { Message } from './Message'
 
 export function MessageList ({ messages }) {
   return (
-    <ul className='MessageList'>
+    <div className='MessageList'>
       {messages.map((message, index) => (
-        <li className={classNames({
-          ...message.metadata,
-          them: 'id' in message,
-          me: !('id' in message)
-        })} key={index}>
-          {message.metadata.think ? '🤔 ' + message.content : message.content}
-        </li>
+        <Message message={message} />
       ))}
-    </ul>
+    </div>
   )
 }
