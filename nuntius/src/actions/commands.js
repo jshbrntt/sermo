91ad87e386a05a591107b7d938fba6d32eeb9928
Joshua Barnett <jbacontrol@gmail.com>
2018-03-08 21:49:59 +0000

    Update commands action to act on seeing an /oops command, supply no args when command was triggered this side of the communication.

--- nuntius/src/actions/commands.js
+++ nuntius/src/actions/commands.js
@@ -1,9 +1,10 @@
-import { sendMessage } from './messages'
+import { sendMessage, removeLastMessage } from './messages'
 
 export const SENT_COMMAND = 'SENT_COMMAND'
 export const RECEIVED_COMMAND = 'RECEIVED_COMMAND'
 export const COMMAND_NICK = 'nick'
 export const COMMAND_THINK = 'think'
+export const COMMAND_OOPS = 'oops'
 
 export const sendCommand = (socket, command, args) => {
   return dispatch => {
@@ -19,6 +20,9 @@ export const sendCommand = (socket, command, args) => {
     switch (command) {
       case COMMAND_THINK:
         sendMessage(socket, args.join(' '), { think: true })(dispatch)
+        break
+      case COMMAND_OOPS:
+        removeLastMessage()(dispatch)
         break
       default:
         window.alert(`Command '${command}' not implemented.`)
