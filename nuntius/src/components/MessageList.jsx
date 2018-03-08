e979f30611975098390b6a2d9711c6b8e9500fd0
Joshua Barnett <jbacontrol@gmail.com>
2018-03-08 22:48:11 +0000

    Make MessageList scrollable and automatically scroll to the bottom upon a change in the messages array length.

--- nuntius/src/components/MessageList.jsx
+++ nuntius/src/components/MessageList.jsx
@@ -2,12 +2,31 @@ import './MessageList.scss'
 import React from 'react'
 import { Message } from './Message'
 
-export function MessageList ({ messages }) {
-  return (
-    <div className='MessageList'>
-      {messages.map((message, index) => (
-        <Message message={message} />
-      ))}
-    </div>
-  )
+export class MessageList extends React.Component {
+  constructor (props) {
+    super(props)
+    this.onScroll = this.onScroll.bind(this)
+  }
+  onScroll (event) {
+    console.log(this.refs.elem.scrollTop)
+  }
+  scrollToBottom () {
+    this.refs.elem.scrollTop = this.refs.elem.offsetHeight
+  }
+  componentDidUpdate (prevProps, prevState) {
+    if (this.props.messages.length !== prevProps.messages.length) {
+      this.scrollToBottom()
+    }
+  }
+  render () {
+    return (
+      <div ref='elem' className='MessageList' onScroll={this.onScroll}>
+        <div className='Messages'>
+          {this.props.messages.map((message, index) => (
+            <Message key={index} message={message} />
+          ))}
+        </div>
+      </div>
+    )
+  }
 }
